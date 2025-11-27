f3492eb4cffe0ddbb2107711ef24583c9885c80b
divyakvta <divyawnd@gmail.com>
2025-11-27 20:47:04 +0530

    portfoio commit - updated imge url

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from './About.module.css';
 import { getImageUrl } from '../../utils';
 import skills from '../../data/skills.json';
+import aboutImage from "../../assets/about/aboutImage.jpg";
 
 const About = () => {
   return (
    <section className={styles.container} id="about">
     <h2 className={styles.title}>About Me</h2>
     <div className={styles.content}>
-        <img src={getImageUrl("about/aboutImage.jpg")} alt="about-image" className={styles.aboutImage} />
+        <img src={aboutImage} alt="about-image" className={styles.aboutImage} />
         <ul className={styles.aboutItems}>
             {skills.map((skill, id) => (
                 <li key={id} className={styles.aboutItem}>
